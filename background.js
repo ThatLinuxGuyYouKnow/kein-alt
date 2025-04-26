4531957446c88ac30c74dfe243114cac64a0a4de
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-04-26 03:22:27 +0100

    fix: now removing overlay if user is not logged in

--- background.js
+++ background.js
@@ -14,25 +14,14 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   }
 
   if (changeInfo.status === 'complete' && (tab.url.includes('twitter.com') || tab.url.includes('x.com'))) {
-    // Wait a bit for dynamic content to load before checking
+
     setTimeout(() => {
       chrome.scripting.executeScript({
         target: { tabId },
         func: () => {
-          const existingOverlay = Array.from(document.querySelectorAll('div'))
-            .find(div =>
-              div.style.backgroundColor === 'black' &&
-              div.style.position === 'fixed' &&
-              div.style.top === '0px' &&
-              div.style.left === '0px' &&
-              div.style.width === '100%' &&
-              div.style.height === '100%' &&
-              div.style.zIndex === '10000'
-            );
-
-
 
           const overlay = document.createElement('div');
+          overlay.className = 'overlay-div';
           Object.assign(overlay.style, {
             backgroundColor: 'black',
             position: 'fixed',
@@ -42,29 +31,27 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
             height: '100%',
             zIndex: '10000',
           });
+
           document.body.appendChild(overlay);
 
 
 
 
+          const existingOverlay = document.querySelector('.overlay-div');
           const profileBtn = Array.from(document.querySelectorAll('span'))
             .find(el => el.textContent.trim() === 'Likes' || el.textContent.trim() === 'Edit profile');
 
           const isLoggedIn = profileBtn
 
-
           if (isLoggedIn) {
             alert('User is logged in');
-            // Keep overlay if logged in
           } else {
-
-            // Remove overlay if not logged in
-            if (existingOverlay) existingOverlay.remove();
+            existingOverlay.replaceWith("")
             alert('User is not logged in');
           }
         }
       },
       );
-    }, 5000); // 2-second delay to ensure dynamic content loads
+    }, 10000);
   }
 });
